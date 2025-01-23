87b536b74e30e6d38464a43b32cc28a6d3b9cbbd
Jaeha Lee <132727115+Jaeha0526@users.noreply.github.com>
2025-01-23 00:30:25 -0800

    servertest

--- server_test.py
+++ server_test.py
@@ -8,4 +8,4 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9001)
+    uvicorn.run(app, host="0.0.0.0", port=9003)
