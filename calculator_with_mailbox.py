1169796eaa31df2f0a60c25143923f035c8ca26a
Jaeha Lee <132727115+Jaeha0526@users.noreply.github.com>
2025-01-30 22:51:10 -0800

    change port

--- calculator_with_mailbox.py
+++ calculator_with_mailbox.py
@@ -159,4 +159,4 @@ async def get_mailbox_entry(index: int):
         }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9019)
+    uvicorn.run(app, host="0.0.0.0", port=9012)
